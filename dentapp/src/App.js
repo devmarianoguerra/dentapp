c8847f517285cafca2c45add0398587cd991be2a
Mariano Guerra <devmarianoguerra@gmail.com>
2020-05-29 12:51:00 -0500

    created login input component and exported in app.js doc

--- dentapp/src/App.js
+++ dentapp/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import LoginButton from "./components/loginButton";
+import LoginInput from "./components/loginInput";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <LoginButton />
+      <LoginInput />
     </>
   );
 }
---
+++ dentapp/src/components/loginInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class LoginInput extends React.Component {
+  render() {
+    return <h1>Login Input</h1>;
+  }
+}
+
+export default LoginInput;
