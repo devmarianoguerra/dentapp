fc66e432b89adddc2e7b752b936ee44d1895acc0
Mariano Guerra <devmarianoguerra@gmail.com>
2020-06-01 14:07:52 -0500

    attempt to change input style using jsx style property

--- dentapp/src/components/loginInput.js
+++ dentapp/src/components/loginInput.js
@@ -1,8 +1,49 @@
 import React from "react";
+import { InputGroup, FormControl } from "react-bootstrap";
+//import "./loginInput.css";
 
 class LoginInput extends React.Component {
+  state = {
+    input: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
+    console.log("input value: ", e.target.value);
+  };
+
   render() {
-    return <h1>Login Input</h1>;
+    return (
+      <>
+        <style type="text/css">
+          {`.inputGroupStyle = {
+              width: 311,
+              height: 53,
+            };
+          
+            .fromControlStyle = {
+              width: 311,
+            height: 53,
+            background-color: #ffff, 
+            color: #d6d6d6, 
+            border: 2px solid #00d9cc, 
+            border-radius: 25,
+            font-family: Roboto, 
+            letter-spacing: 0.0357143em,
+            padding: 10px 18px 0px 25px, 
+            }`}
+        </style>
+        <InputGroup className="inputGroupStyle">
+          <div>
+            <FormControl
+              className="formControlStyle"
+              placeholder="contact@email.com"
+              onChange={this.handleChange}
+            />
+          </div>
+        </InputGroup>
+      </>
+    );
   }
 }
 
