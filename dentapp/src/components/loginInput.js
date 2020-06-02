08c684fa7b5557368b851bb15cf465767e341c47
Mariano Guerra <devmarianoguerra@gmail.com>
2020-06-01 20:00:42 -0500

    changed the style properties to meet the app requirement

--- dentapp/src/components/loginInput.js
+++ dentapp/src/components/loginInput.js
@@ -15,28 +15,19 @@ class LoginInput extends React.Component {
   render() {
     return (
       <>
-        <style type="text/css">
-          {`.inputGroupStyle = {
-              width: 311,
-              height: 53,
-            };
-          
-            .fromControlStyle = {
-              width: 311,
-            height: 53,
-            background-color: #ffff, 
-            color: #d6d6d6, 
-            border: 2px solid #00d9cc, 
-            border-radius: 25,
-            font-family: Roboto, 
-            letter-spacing: 0.0357143em,
-            padding: 10px 18px 0px 25px, 
-            }`}
-        </style>
         <InputGroup className="inputGroupStyle">
           <div>
             <FormControl
-              className="formControlStyle"
+              style={{
+                width: 311,
+                height: 53,
+                fontSize: 18,
+                color: "#929292",
+                borderColor: "#00D9CC",
+                borderWidth: 1.5,
+                borderRadius: 60,
+                fontFamily: "Roboto",
+              }}
               placeholder="contact@email.com"
               onChange={this.handleChange}
             />
