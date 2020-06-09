e817601ce64e950d8f1a6b149d02e34d0e287e8d
Mariano Guerra <devmarianoguerra@gmail.com>
2020-06-09 11:57:23 -0500

    Changed styles outside function. Added PropTypes

--- dentapp/src/components/loginButton.js
+++ dentapp/src/components/loginButton.js
@@ -1,32 +1,43 @@
 import React from "react";
 import Styled from "styled-components";
+import PropTypes from "prop-types";
+
+const white = "#ffff";
+const cerulean = "#00ADC1";
+const mint = "#DBF8FA";
+const cyan = "#00d9cc";
+const primaryFont = "Roboto";
+
+const Button = Styled.button`
+background-color: ${(props) => (props.primary ? cyan : mint)};
+color: ${(props) => (props.primary ? white : cerulean)};
+border: none;
+border-radius: 60px;
+font-family: ${(props) => (props.primary ? primaryFont : primaryFont)};
+letter-spacing: 0.0357143em;
+text-transform: uppercase;
+width: 311px;
+height: 53px;
+`;
 
 function LoginButton() {
-  const handleClick = () => {
-    console.log("The button was clicked");
-  };
-
-  const Button = Styled.button`
-  background-color: ${(props) => (props.primary ? "#DBF8FA" : "#00d9cc")};
-  color: ${(props) => (props.primary ? "#00ADC1" : "#ffff")};
-  border: none;
-  border-radius: 60px;
-  font-family: ${(props) => (props.primary ? "Roboto" : "Roboto")};
-  letter-spacing: 0.0357143em;
-  text-transform: uppercase;
-  width: 311px;
-  height: 53px;
-  `;
-
   return (
     <>
-      <Button primary onClick={handleClick}>
-        Accesar
+      <Button primary onClick={props.handleClick}>
+        {props.action}
       </Button>
-
-      <Button onClick={handleClick}>Enviar</Button>
     </>
   );
 }
 
 export default LoginButton;
+
+Button.PropTypes = {
+  onClick: PropTypes.function.isRequired,
+  action: PropTypes.string.isRequired,
+  theme: PropTypes.string,
+};
+
+Button.defaultProps = {
+  theme: "default",
+};
