01e7620a00c548ec104a80f4cdfd6f123771835b
Oscar Nunez <taw.oscarn@gmail.com>
2020-06-11 19:19:54 -0600

    Minor changes -

--- dentapp/src/App.js
+++ dentapp/src/App.js
@@ -5,9 +5,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/js/dist/index.js";
 
 import Header from "./components/Header";
-import LoginLogo from "./components/LoginLogo";
-// import LoadingSpinner from "./components/LoadingSpinner";
-import TextFeedback from "./components/TextFeedback";
 
 function App(props) {
   const primaryColor = "#00D9CC";
@@ -18,16 +15,10 @@ function App(props) {
         backgroundColor={primaryColor}
         iconAndTitleColor={secundaryColor}
         routeToNavigate="/"
-      >
-        <LoginLogo />
-      </Header>
-
+      ></Header>
       <Switch>
-        <Route path="/">
-          <TextFeedback status="success" text="Acabamos de enviarte un mail. Haz click en el enlace para reestablecer tu contraseña."/>
-          {/* <LoadingSpinner show /> */}
-        </Route>
-      </Switch>
+        <Route path="/"></Route>{" "}
+      </Switch>{" "}
     </BrowserRouter>
   );
 }
--- dentapp/src/components/TextFeedback.js
+++ dentapp/src/components/TextFeedback.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Container from "react-bootstrap/Container";
-import Success from "../assets/Success.svg";
+import SuccessIcon from "../assets/Success.svg";
 
 const TextContainer = styled.div`
   color: ${(props) => props.color};
@@ -14,29 +14,32 @@ class TextFeedback extends Component {
   state = {
     status: this.props.status,
     text: this.props.text,
+    title: this.props.title,
   };
 
   render() {
-    const { status, text } = this.state;
+    const { status, text, title } = this.state;
+    const primaryColor = "#00D9CC"; //green color
+    const secundaryColor = "#70757af0"; //grey color
+    const errorColor = "#FF5555"; //red color
     return (
       <>
         {status === "success" && (
           <Container className="text-center">
-            <img src={Success} alt="Success" />
+            <img src={SuccessIcon} alt="Success icon" />
 
-            <TextContainer color="#00D9CC">
-              <h1>¡Listo!</h1>
+            <TextContainer color={primaryColor}>
+              <h1>{title}</h1>
             </TextContainer>
 
-            <TextContainer color="#70757af0">
+            <TextContainer color={secundaryColor}>
               <p>{text}</p>
             </TextContainer>
-
           </Container>
         )}
 
         {status === "error" && (
-          <TextContainer error color="#FF5555">
+          <TextContainer error color={errorColor}>
             <p>{text}</p>
           </TextContainer>
         )}
@@ -48,6 +51,11 @@ class TextFeedback extends Component {
 TextFeedback.propTypes = {
   status: PropTypes.string,
   text: PropTypes.string,
+  title: PropTypes.string,
+};
+
+TextFeedback.defaultProps = {
+  title: "¡Listo!",
 };
 
 export default TextFeedback;
