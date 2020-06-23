a8460d218a97b86af647b56aae4867db58a1ae49
Oscar Nunez <taw.oscarn@gmail.com>
2020-06-23 01:57:21 -0600

    Cleanning App.js

--- dentapp/src/App.js
+++ dentapp/src/App.js
@@ -5,9 +5,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/js/dist/index.js";
 
 import Header from "./components/Header";
-import DesechablesIcon from './assets/DesechablesIcon.svg'
-
-import Category from "./components/Category";
 
 function App(props) {
   const primaryColor = "#00D9CC";
@@ -19,15 +16,10 @@ function App(props) {
         iconAndTitleColor={secundaryColor}
         routeToNavigate="/"
       >
-        {/* <LoginLogo /> */}
       </Header>
 
       <Switch>
         <Route path="/">
-          <Category title="Desechables" bgColor="#57edf799" icon={DesechablesIcon} />
-          <Category title="Estuches" bgColor="#00d9cc99" icon={DesechablesIcon} />
-          <Category title="Desechables" bgColor="#57edf799" icon={DesechablesIcon} />
-
         </Route>
       </Switch>
     </BrowserRouter>
