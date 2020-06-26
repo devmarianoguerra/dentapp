ec8e40e2ae90b129c40e2a4a7949e8cee63a4675
Mariano Guerra <devmarianoguerra@gmail.com>
2020-06-26 14:19:06 -0500

    added PropTypes to buttons

--- dentapp/src/components/UserButton.js
+++ dentapp/src/components/UserButton.js
@@ -1,23 +1,50 @@
 import React from "react";
 import { FaUserAlt } from "react-icons/fa";
 import Styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Icon = Styled.button`
-border-radius: 50px;
-border: 1px solid #00D9CC;
-background-color: transparent;
-color: #00D9CC;
-margin: 5px;
-padding: 6px 12px 11px 12px ;
-font-size: 16px;
+border-radius: ${(props) => props.borderRadius};
+border: ${(props) => props.border};
+background-color: ${(props) => props.backgroundColor};
+color: ${(props) => props.color};
+padding: ${(props) => props.padding} ;
+font-size: ${(props) => props.fontSize};
 `;
 
 function UserButton() {
+  const {
+    backgroundColor,
+    borderRadius,
+    border,
+    color,
+    padding,
+    fontSize,
+    onClick,
+  } = this.props;
   return (
-    <Icon onClick={props.onClick}>
+    <Icon
+      onClick={onClick}
+      backgroundColor={backgroundColor}
+      borderRadius={borderRadius}
+      border={border}
+      color={color}
+      padding={padding}
+      fontSize={fontSize}
+    >
       <FaUserAlt />
     </Icon>
   );
 }
 
 export default UserButton;
+
+UserButton.propTypes = {
+  backgroundColor: PropTypes.string.isRequired,
+  borderRadius: PropTypes.string,
+  border: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  padding: PropTypes.string.isRequired,
+  fontSize: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
--- dentapp/src/components/shoppingButton.js
+++ dentapp/src/components/shoppingButton.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { MdShoppingCart } from "react-icons/md";
 import Styled from "styled-components";
+import Proptypes from "prop-types";
 
 const Icon = Styled.button`
-border: none;
-background-color: transparent;
-color: #4A4A4A;
-font-size: 20px;
+border: ${(props) => props.border};
+background-color: ${(props) => props.backgroundColor};
+color: ${(props) => props.color};
+font-size: ${(props) => props.fontSize};
 `;
 
 function ShoppingButton() {
+  const { backgroundColor, color, border, onClick, fontSize } = this.props;
   return (
-    <Icon onClick={props.onClick}>
+    <Icon
+      onClick={onClick}
+      backgroundColor={backgroundColor}
+      color={color}
+      border={border}
+      fontSize={fontSize}
+    >
       <MdShoppingCart />
     </Icon>
   );
 }
 
 export default ShoppingButton;
+
+ShoppingButton.propTypes = {
+  backgroundColor: Proptypes.string.isRequired,
+  color: Proptypes.string.isRequired,
+  border: Proptypes.string.isRequired,
+  onClick: Proptypes.func.isRequired,
+  fontSize: Proptypes.string.isRequired,
+};
